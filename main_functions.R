cbec45d6234fbe82ecc4a8eae2cceb5c8e09c7ba
Andy Zack <andybzack@gmail.com>
2024-03-12 16:20:33 -0600

    move main code into funcions

---
+++ main_functions.R
@@ -0,0 +1,72 @@
+get_and_run_sql <- function(q, assistant_id, shared_vars) {
+  
+  cat("got input...")
+  shared_vars$thread_id <- start_thread_with_question(q)
+  thread_id <- shared_vars$thread_id
+  
+  cat(paste0("started thread (", thread_id, ")..."))
+  run_id <- create_run_for_thread(assistant_id, thread_id)
+  
+  cat("started run...")
+  messages <- poll_for_response(run_id, thread_id)
+  
+  cat("got messages...")
+  
+  if (!startsWith(messages$data[[1]]$content[[1]]$text$value, "ERROR:")) {
+    
+    cat("no error found...")
+    sql_query <- extract_sql_query_from_messages(messages)
+    cat("extracted sql...")
+    
+    # Connect to database
+    con <- dbConnect(SQLite(), dbname = here("Chinook_Sqlite.sqlite"))
+    
+    # Execute the SQL query and fetch results
+    result <- dbGetQuery(con, sql_query)
+    shared_vars$result <- result
+    
+    # Disconnect from the database
+    dbDisconnect(con)
+    
+  } else {
+ 
+    cat("did not return SQL...")
+    result <- data.frame()
+    sql_query <- messages$data[[1]]$content[[1]]$text$value
+   
+  }
+  
+  return(list(result = result,
+              sql_query = sql_query))
+}
+
+get_and_run_r <- function(q, assistant_id, thread_id, shared_vars) {
+  thread_id <- shared_vars$thread_id
+  result <- shared_vars$result
+  
+  cat(thread_id)
+  add_message_to_thread(thread_id, q)
+  cat("added message to thread...")
+  
+  run_id <- create_run_for_thread(assistant_id, thread_id)
+  cat("started run...")
+  
+  messages <- poll_for_response(run_id, thread_id)
+  cat("got messages...")
+
+  message <- get_most_recent_assistant_text(messages)
+  cat(message)
+  cat("got most recent...")
+
+  r_code <- extract_r_from_message(message)
+  cat("got r code...")
+  cat(r_code)
+  
+  plot_expr <- parse(text = paste("disp_plot <-", r_code))
+  cat("parsed r code")
+  
+  eval(plot_expr)
+  cat("evaluated r code...")
+  
+  return(disp_plot)
+}
--- server.R
+++ server.R
@@ -5,103 +5,30 @@ library(jsonlite)
 library(here)
 library(DT)
 library(ggplot2)
+library(dplyr)
 
 server <- function(input, output, session) {
 
   source(here::here("api_helpers.R"))
+  source(here::here("main_functions.R"))
   assistant_id <- "asst_xWnXfPj1aNLlCXF1nZXAAd7p" # Replace with your assistant's ID
-  shared_vars <- reactiveValues(thread_id = NULL,
-                                result = NULL)
+  shared_vars <- reactiveValues(thread_id = NULL,result = NULL)
 
-  cat("got source code...")
-  
   observeEvent(input$submit, {
-    
-    cat("got input...")
-    # Test if API call can be made
-    shared_vars$thread_id <- start_thread_with_question(input$question)
-    thread_id <- shared_vars$thread_id
-    cat(paste0("started thread (", thread_id, ")..."))
-    run_id <- create_run_for_thread(assistant_id, thread_id)
-    cat("started run...")
-    messages <- poll_for_response(run_id, thread_id)
-    cat("got messages...")
-    
-    if (!startsWith(messages$data[[1]]$content[[1]]$text$value, "ERROR:")) {
-      
-      cat("no error found...")
-      sql_query <- extract_sql_query_from_messages(messages)
-      cat("extracted sql...")
-      
-      # Connect to database
-      con <- dbConnect(SQLite(), dbname = here("Chinook_Sqlite.sqlite"))
-      
-      # Execute the SQL query and fetch results
-      result <- dbGetQuery(con, sql_query)
-      shared_vars$result <- result
-      
-      # Disconnect from the database
-      dbDisconnect(con)
-      
-      # Output the query results
-      output$resultOutput <- DT::renderDataTable({
-        result
-      })
-      
-      output$sqlOutput <- renderText({
-        sql_query
-      })
-      
-    } else {
-      
-      cat("did not return SQL...")
-      # Output the query results
-      output$resultOutput <- DT::renderDataTable({
-        data.frame()
-      })
-      
-      output$sqlOutput <- renderText({
-        messages$data[[1]]$content[[1]]$text$value
-      })
-      
-    }
-    
-    # Output the SQL query
-    output$questionOutput <- renderText({
-      input$question
-    })
-    
+    result_list <- get_and_run_sql(q = input$question,
+                    assistant_id = assistant_id,
+                    shared_vars = shared_vars)
+    output$resultOutput <- DT::renderDataTable({result_list$result})
+    output$sqlOutput <- renderText({result_list$sql_query})
+    output$questionOutput <- renderText({input$question})
   })
+  
   observeEvent(input$submit_graph, {
-    
-    cat("got graph input...")
-    
-    #get global reactive vars from earlier
-    thread_id <- shared_vars$thread_id
-    result <- shared_vars$result
-    
-    # add new message to existing thread
-    cat(thread_id)
-    add_message_to_thread(thread_id, input$graph_question)
-    add_message_to_thread(thread_id, input$graph_question)
-    cat("added message to thread...")
-    run_id <- create_run_for_thread(assistant_id, thread_id)
-    cat("started run...")
-    messages <- poll_for_response(run_id, thread_id)
-    cat("got messages...")
-    # get most recent message
-    message <- get_most_recent_assistant_text(messages)
-    cat(message)
-    cat("got most recent...")
-    # extract the R code
-    r_code <- extract_r_from_message(message)
-    cat("got r code...")
-    cat(r_code)
-    plot_expr <- parse(text = paste("disp_plot <-", r_code))
-    cat("parsed r code")
-    eval(plot_expr)
-    cat("evaluated r code...")
-    #output plot
+    disp_plot <- get_and_run_r(q = input$graph_question, 
+                               assistant_id = assistant_id, 
+                               thread_id = thread_id,
+                               shared_vars = shared_vars)
+      
     output$plotOutput <- renderPlot({
       disp_plot
     })
